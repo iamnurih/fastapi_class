cf1d4036593d8d9dd23dff0a7916c9334619f231
iamnurih <emailnuri@gmail.com>
2025-09-30 10:13:13 +0900

    install mypy

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+from typing import Any
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root() -> dict[str, str]:
     return {"message": "Hello World"}
 
 
 @app.get("/hello/{name}")
-async def say_hello(name: str):
+async def say_hello(name: str) -> dict[str, str]:
     return {"message": f"Hello {name}"}
