682358f6b05af8961379018e1bc30fb405189e71
iamnurih <emailnuri@gmail.com>
2025-10-01 16:18:57 +0900

    deleted unnecessary space for black

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from app.apis.v1.meeting_router import edgedb_router as meeting_edgedb_router
 from app.apis.v1.meeting_router import mysql_router as meeting_mysql_router
 
 app = FastAPI(
-    default_response_class= ORJSONResponse
+    default_response_class=ORJSONResponse
 )
 
 app.include_router(meeting_edgedb_router)
